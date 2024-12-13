60d663f07005a5d284cf2ee34b611de6233cadd5
Eoghanmc22 <eoghanmc@icloud.com>
2024-12-13 14:47:50 -0500

    Change waterlinked ip as to not conflict with the address of the NIC for the rov

--- waterlinked/src/waterlinked.rs
+++ waterlinked/src/waterlinked.rs
@@ -40,7 +40,7 @@ fn start_task(runtime: Res<TokioTasksRuntime>) {
     runtime.spawn_background_task(|mut ctx| async move {
         let mut interval = tokio::time::interval(Duration::from_secs_f64(1.0 / 4.0));
         // let api = WaterLinked::new("https://demo.waterlinked.com/".try_into().unwrap());
-        let api = WaterLinked::new("192.168.2.94".try_into().unwrap());
+        let api = WaterLinked::new("192.168.3.3".try_into().unwrap());
 
         loop {
             interval.tick().await;
