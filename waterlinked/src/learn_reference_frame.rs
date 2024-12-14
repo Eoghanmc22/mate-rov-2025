0968f831f42d07ea34a7721a9a632c79d81267e1
Eoghanmc22 <eoghanmc@icloud.com>
2024-12-14 10:27:53 -0500

    Fix panic in learn_reference_frame

--- waterlinked/src/learn_reference_frame.rs
+++ waterlinked/src/learn_reference_frame.rs
@@ -21,16 +21,21 @@ pub struct LearnReferenceFramePlugin;
 
 impl Plugin for LearnReferenceFramePlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<LearningState>().add_systems(
-            Update,
-            (
-                update_state.after(waterlinked::pose_updater),
-                latency_estimator.after(update_state),
-                learn_reference_frame.after(latency_estimator),
-            ),
-        );
+        app.init_resource::<LearningState>()
+            .add_systems(
+                Update,
+                (
+                    update_state.after(waterlinked::pose_updater),
+                    latency_estimator.after(update_state),
+                    learn_reference_frame.after(latency_estimator),
+                ),
+            )
+            .add_event::<FullStateChangeNotif>();
     }
 }
+
+#[derive(Event)]
+struct FullStateChangeNotif;
 
 #[derive(Debug, Default, PartialEq, Eq)]
 enum VelocityState {
@@ -85,7 +90,10 @@ impl Default for LearningState {
 fn latency_estimator(mut state: ResMut<LearningState>) {
     if state.waterlinked_state.state == VelocityState::Moving
         && state.robot_state.state == VelocityState::Still
-        && state.waterlinked_state.last_time - state.robot_state.start_time
+        && state
+            .waterlinked_state
+            .last_time
+            .saturating_sub(state.robot_state.start_time)
             > TIME_EPSILON + state.position_latency_estimate
     {
         warn!("State miss match, waterlinked reports moving while robot is meant to be still");
@@ -95,8 +103,11 @@ fn latency_estimator(mut state: ResMut<LearningState>) {
     if state.waterlinked_state.state == state.robot_state.state
         && state.waterlinked_state.just_changed
     {
-        let latency_secs =
-            (state.waterlinked_state.start_time - state.robot_state.start_time).as_secs_f32();
+        let latency_secs = (state
+            .waterlinked_state
+            .start_time
+            .saturating_sub(state.robot_state.start_time))
+        .as_secs_f32();
         let old_latency_secs = state.position_latency_estimate.as_secs_f32();
 
         let new_latency_estimate =
@@ -113,6 +124,7 @@ fn update_state(
     mut state: ResMut<LearningState>,
     time: Res<Time<Real>>,
     query: Query<(&CurrentPose, &ActualMovement, &Orientation), With<Robot>>,
+    mut events: EventWriter<FullStateChangeNotif>,
 ) {
     let Ok((CurrentPose(pose), ActualMovement(movement), Orientation(quat))) = query.get_single()
     else {
@@ -149,7 +161,10 @@ fn update_state(
         state
             .robot_velocity_buffer
             .retain(|(_velo, _thrust, time)| {
-                *time > current_time - latency_estimate - TIME_EPSILON
+                *time
+                    > current_time
+                        .saturating_sub(latency_estimate)
+                        .saturating_sub(TIME_EPSILON)
             });
 
         if velocity.norm_squared() > VELOCITY_THRESHOLD * VELOCITY_THRESHOLD {
@@ -161,6 +176,8 @@ fn update_state(
                 .waterlinked_state
                 .observe(VelocityState::Still, current_time);
         }
+
+        events.send(FullStateChangeNotif);
     }
 
     *last_waterlinked_pose = Some((*pose, current_time));
@@ -170,7 +187,13 @@ fn learn_reference_frame(
     time: Res<Time<Real>>,
     state: Res<LearningState>,
     mut reference_angle: ResMut<WaterlinkedAngleOffset>,
+    mut events: EventReader<FullStateChangeNotif>,
 ) {
+    if events.is_empty() {
+        return;
+    }
+    events.clear();
+
     let current_time = time.elapsed();
     if !(state.robot_state.state == VelocityState::Moving
         && state.waterlinked_state.state == VelocityState::Moving
@@ -180,9 +203,9 @@ fn learn_reference_frame(
     }
 
     let thrust = state.robot_velocity_buffer.last();
-    let velo_idx = state
-        .robot_velocity_buffer
-        .partition_point(|(_, _, time)| *time < current_time - state.position_latency_estimate);
+    let velo_idx = state.robot_velocity_buffer.partition_point(|(_, _, time)| {
+        *time < current_time.saturating_sub(state.position_latency_estimate)
+    });
     let velo = state.robot_velocity_buffer.get(velo_idx);
 
     if let (Some((velo, _, _)), Some((_, thrust, _))) = (velo, thrust) {
@@ -190,11 +213,20 @@ fn learn_reference_frame(
         let observed_rotation = Quat::from_rotation_arc_2d(*velo, *thrust);
 
         if USE_REFERENCE_FRAME_LEARNING {
-            if Quat::angle_between(reference_angle.0, observed_rotation) < LEARN_MAX_DIFFERENCE {
+            let old_ref_angle = reference_angle.0;
+
+            if Quat::angle_between(reference_angle.0, observed_rotation).abs()
+                < LEARN_MAX_DIFFERENCE
+            {
                 reference_angle.0 = reference_angle.0.slerp(observed_rotation, LEARN_ALPHA);
             } else {
                 reference_angle.0 = observed_rotation;
             }
+
+            info!(
+                "Rotated reference frame by: {:.2} deg",
+                Quat::angle_between(old_ref_angle, reference_angle.0).to_degrees()
+            )
         }
     }
 }
