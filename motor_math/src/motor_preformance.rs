42055924c44491c53e91a83cc604e5f9f3c7fcbb
Eoghanmc22 <eoghanmc@icloud.com>
2025-02-03 09:59:50 -0500

    Use lookup table in motor_performance

--- motor_math/src/motor_preformance.rs
+++ motor_math/src/motor_preformance.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 
 use anyhow::Context;
+use itertools::Itertools;
 use serde::Deserialize;
 use tracing::instrument;
 
@@ -33,6 +34,26 @@ impl MotorData {
     }
 
     #[instrument(level = "trace", skip(self), ret)]
+    pub fn binary_search_by_force<D: Number>(
+        &self,
+        force: D,
+        interpolation: Interpolation,
+        extrapolate: bool,
+    ) -> MotorRecord<D> {
+        let nearest_records = self.force_index.binary_search_nearest(force.re());
+
+        Self::interpolate(
+            nearest_records.0,
+            nearest_records.1,
+            force,
+            nearest_records.0.force,
+            nearest_records.1.force,
+            interpolation,
+            extrapolate,
+        )
+    }
+
+    #[instrument(level = "trace", skip(self), ret)]
     pub fn lookup_by_current<D: Number>(
         &self,
         signed_current: D,
@@ -40,6 +61,28 @@ impl MotorData {
         extrapolate: bool,
     ) -> MotorRecord<D> {
         let nearest_records = self.current_index.lookup_nearest(signed_current.re());
+
+        Self::interpolate(
+            nearest_records.0,
+            nearest_records.1,
+            signed_current,
+            nearest_records.0.current.copysign(nearest_records.0.force),
+            nearest_records.1.current.copysign(nearest_records.1.force),
+            interpolation,
+            extrapolate,
+        )
+    }
+
+    #[instrument(level = "trace", skip(self), ret)]
+    pub fn binary_search_by_current<D: Number>(
+        &self,
+        signed_current: D,
+        interpolation: Interpolation,
+        extrapolate: bool,
+    ) -> MotorRecord<D> {
+        let nearest_records = self
+            .current_index
+            .binary_search_nearest(signed_current.re());
 
         Self::interpolate(
             nearest_records.0,
@@ -130,13 +173,13 @@ impl From<Vec<MotorRecord<FloatType>>> for MotorData {
 
 #[derive(Clone, Copy, PartialEq, Eq, Default, Debug)]
 pub enum Interpolation {
-    /// Return the linear interpolation betwwn the two data entries closest to the the requested data point
+    /// Return the linear interpolation between the two data entries closest to the the requested data point
     /// and modifies the pwm field to match the direction of the propeller
     LerpDirection(Direction),
     /// Return the raw data entry closest to the the requested data point
     /// Only modifies the pwm field to match the direction of the propeller
     Direction(Direction),
-    /// Return the linear interpolation betwwn the two data entries closest to the the requested data point
+    /// Return the linear interpolation between the two data entries closest to the the requested data point
     #[default]
     Lerp,
     /// Return the raw data entry closest to the the requested data point
@@ -144,7 +187,7 @@ pub enum Interpolation {
     OriginalData,
 }
 
-#[derive(Deserialize, Debug, Clone, Copy)]
+#[derive(Deserialize, Debug, Clone, Copy, Default, PartialEq)]
 pub struct MotorRecord<D> {
     pub current: D,
     pub force: D,
@@ -221,7 +264,38 @@ pub fn read_motor_data_from_string(data: &str) -> anyhow::Result<MotorData> {
 
 struct RecordIndex {
     data: Vec<MotorRecord<FloatType>>,
+    lookup_table: Box<
+        [(
+            MotorRecord<FloatType>,
+            Option<MotorRecord<FloatType>>,
+            MotorRecord<FloatType>,
+        )],
+    >,
+    float_compression: FloatCompression,
     supplier: Box<dyn Fn(&MotorRecord<FloatType>) -> FloatType + Send + Sync + 'static>,
+}
+
+#[derive(Debug)]
+struct FloatCompression {
+    min: FloatType,
+    max: FloatType,
+    steps: isize,
+}
+
+impl FloatCompression {
+    pub fn compress(&self, float: FloatType) -> isize {
+        // ((float - self.min) / (self.max - self.min) * (self.steps - 1) as FloatType) as isize
+        ((float - self.min) / (self.max - self.min) * (self.steps - 1) as FloatType).floor()
+            as isize
+    }
+    pub fn decompress(&self, idx: isize) -> (FloatType, FloatType) {
+        (
+            (idx as FloatType + 0.0) / (self.steps - 1) as FloatType * (self.max - self.min)
+                + self.min,
+            (idx as FloatType + 1.0) / (self.steps - 1) as FloatType * (self.max - self.min)
+                + self.min,
+        )
+    }
 }
 
 impl RecordIndex {
@@ -229,24 +303,161 @@ impl RecordIndex {
         data: Vec<MotorRecord<FloatType>>,
         supplier: impl Fn(&MotorRecord<FloatType>) -> FloatType + Send + Sync + 'static,
     ) -> Self {
+        assert!(
+            data.len() >= 2,
+            "The data in a record index needs to have at least two elements"
+        );
+
+        let min = data.first().map(&supplier).unwrap();
+        let max = data.last().map(&supplier).unwrap();
+
+        let min_step_size = data
+            .iter()
+            .map(&supplier)
+            .tuple_windows()
+            .map(|(a, b)| b - a)
+            .min_by(FloatType::total_cmp)
+            .unwrap();
+        let steps = ((max - min) / min_step_size).ceil() as usize + 1;
+
+        let compression = FloatCompression {
+            min,
+            max,
+            steps: steps as isize,
+        };
+
+        let mut lookup_table = Vec::with_capacity(steps);
+        for step in 0..steps {
+            let (low_value, high_value) = compression.decompress(step as isize);
+            let (low, mid1) = binary_search_nearest_internal(low_value, &data, &supplier);
+            let (mid2, high) = binary_search_nearest_internal(high_value, &data, &supplier);
+
+            let mid = if mid1 == mid2 {
+                Some(*mid1)
+            } else {
+                assert!((supplier)(mid1) > (supplier)(mid2), "Data point skipped");
+                None
+            };
+
+            lookup_table.push((*low, mid, *high));
+        }
+
         Self {
             data,
+            lookup_table: lookup_table.into_boxed_slice(),
+            float_compression: compression,
             supplier: Box::new(supplier),
         }
+    }
+
+    pub fn binary_search_nearest(
+        &self,
+        val: FloatType,
+    ) -> (&MotorRecord<FloatType>, &MotorRecord<FloatType>) {
+        binary_search_nearest_internal(val, &self.data, &self.supplier)
     }
 
     pub fn lookup_nearest(
         &self,
         val: FloatType,
     ) -> (&MotorRecord<FloatType>, &MotorRecord<FloatType>) {
-        let partition_point = self.data.partition_point(|x| (self.supplier)(x) < val);
+        let idx =
+            (self.float_compression.compress(val).max(0) as usize).min(self.lookup_table.len() - 1);
+        let (low, mid, high) = &self.lookup_table[idx];
 
-        let idx_b = partition_point.max(1).min(self.data.len() - 1);
-        let idx_a = idx_b - 1;
+        if let Some(mid) = mid {
+            if ((self.supplier)(low)..=(self.supplier)(mid)).contains(&val) {
+                (low, mid)
+            } else {
+                (mid, high)
+            }
+        } else {
+            (low, high)
+        }
+    }
+}
 
-        let a = &self.data[idx_a];
-        let b = &self.data[idx_b];
+fn binary_search_nearest_internal(
+    val: FloatType,
+    data: &[MotorRecord<FloatType>],
+    supplier: impl Fn(&MotorRecord<FloatType>) -> FloatType + Send + Sync,
+) -> (&MotorRecord<FloatType>, &MotorRecord<FloatType>) {
+    let partition_point = data.partition_point(|x| (supplier)(x) < val);
 
-        (a, b)
+    let idx_b = partition_point.max(1).min(data.len() - 1);
+    let idx_a = idx_b - 1;
+
+    let a = &data[idx_a];
+    let b = &data[idx_b];
+
+    (a, b)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::FloatType;
+
+    #[test]
+    fn check_force_lookup_table() {
+        let motor_data =
+            super::read_motor_data_from_path("../robot/motor_data.csv").expect("Read motor data");
+        let float_compression = &motor_data.force_index.float_compression;
+        let epsilon = 0.0001;
+
+        let steps = 2000;
+        for step in 0..steps {
+            let point = (float_compression.max - float_compression.min) / steps as FloatType
+                * step as FloatType
+                + float_compression.min;
+
+            let lookup = motor_data.lookup_by_force(point, super::Interpolation::Lerp, false);
+            let binary_search =
+                motor_data.binary_search_by_force(point, super::Interpolation::Lerp, false);
+
+            dbg!(&point);
+            dbg!(&lookup.force);
+            dbg!(&lookup.current);
+            dbg!(&binary_search.force);
+            dbg!(&binary_search.current);
+
+            assert!((lookup.force - point).abs() < epsilon);
+            assert!((binary_search.force - point).abs() < epsilon);
+            assert!((binary_search.current - lookup.current).abs() < epsilon);
+            assert_eq!(lookup, binary_search);
+
+            println!();
+        }
+    }
+
+    #[test]
+    fn check_current_lookup_table() {
+        let motor_data =
+            super::read_motor_data_from_path("../robot/motor_data.csv").expect("Read motor data");
+        let float_compression = &motor_data.current_index.float_compression;
+        let epsilon = 0.0001;
+
+        let steps = 2000;
+        for step in 0..steps {
+            let point = (float_compression.max - float_compression.min) / steps as FloatType
+                * step as FloatType
+                + float_compression.min;
+
+            let lookup = motor_data.lookup_by_current(point, super::Interpolation::Lerp, false);
+            let binary_search =
+                motor_data.binary_search_by_current(point, super::Interpolation::Lerp, false);
+
+            dbg!(&point);
+            dbg!(&lookup.force);
+            dbg!(&lookup.current);
+            dbg!(&binary_search.force);
+            dbg!(&binary_search.current);
+
+            assert!((lookup.current.copysign(lookup.force) - point).abs() < epsilon);
+            assert!((binary_search.current.copysign(binary_search.force) - point).abs() < epsilon);
+            assert!((binary_search.force - lookup.force).abs() < epsilon);
+            assert_eq!(lookup, binary_search);
+
+            println!();
+        }
     }
 }
