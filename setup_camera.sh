295b122d09a4535c3d14f635b5561c9b7c60effb
Eoghanmc22 <eoghanmc@icloud.com>
2025-04-28 20:47:48 -0400

    fix exposure

--- setup_camera.sh
+++ setup_camera.sh
@@ -4,3 +4,10 @@ if v4l2-ctl -d $1 -l | grep focus; then
 	v4l2-ctl -d $1 --set-ctrl=focus_automatic_continuous=0
 	v4l2-ctl -d $1 --set-ctrl=focus_absolute=0
 fi
+
+if v4l2-ctl -d $1 -l | grep auto_exposure; then
+	# Manual mode
+	v4l2-ctl -d $1 --set-ctrl=auto_exposure=1
+	# Night
+	v4l2-ctl -d $1 --set-ctrl=exposure_time_absolute=500
+fi
