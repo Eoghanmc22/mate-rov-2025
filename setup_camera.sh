c5c8147aa8ed0aba1a3f5d5fa47128890d3122d7
Eoghanmc22 <eoghanmc@icloud.com>
2025-04-30 16:02:34 -0400

    Re enable camera auto exposure

--- setup_camera.sh
+++ setup_camera.sh
@@ -5,9 +5,15 @@ if v4l2-ctl -d $1 -l | grep focus; then
 	v4l2-ctl -d $1 --set-ctrl=focus_absolute=0
 fi
 
+# Day
 if v4l2-ctl -d $1 -l | grep auto_exposure; then
 	# Manual mode
-	v4l2-ctl -d $1 --set-ctrl=auto_exposure=1
-	# Night
-	v4l2-ctl -d $1 --set-ctrl=exposure_time_absolute=500
+	v4l2-ctl -d $1 --set-ctrl=auto_exposure=3
 fi
+
+# Night
+# if v4l2-ctl -d $1 -l | grep auto_exposure; then
+# 	# Manual mode
+# 	v4l2-ctl -d $1 --set-ctrl=auto_exposure=1
+# 	v4l2-ctl -d $1 --set-ctrl=exposure_time_absolute=500
+# fi
